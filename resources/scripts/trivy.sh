642028b048510707d85c1a7ec3f4f98613f30822
Aryan Mehesare <99786026+mortal22soul@users.noreply.github.com>
2025-09-16 22:53:18 +0530

    fixed typo

--- resources/scripts/trivy.sh
+++ resources/scripts/trivy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo imageNane - $1
+echo imageName - $1
 echo severity - $2
 echo exitCode - $3
 
 trivy image $1 \
     --severity $2 \
     --exit-code $3 \
-    --fornat json -o trivy-inage-$2-resu1ts.json
+    --format json -o trivy-image-$2-results.json
